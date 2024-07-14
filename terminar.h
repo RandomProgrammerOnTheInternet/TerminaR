991f83b2c0ade874a61e902ce8f7434895bcbcb5
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2024-07-14 17:54:39 -0400

    more typos

--- terminar.h
+++ terminar.h
@@ -68,8 +68,8 @@ void trmR_stpx(u16, u16, u8, u8, u8);
 void trmR_show_scr();
 void trmR_end();
 void trmR_lerp_rgb(f32, u8, u8, u8, u8, u8, u8, u8*, u8*, u8*);
-void trmR_draw_line_low(u16, u16, u16, i16, u8, u8, u8, u8, u8, u8);
-void trmR_draw_line_high(u16, u16, u16, i16, u8, u8, u8, u8, u8, u8);
+void trmR_draw_line_low(u16, u16, u16, u16, u8, u8, u8, u8, u8, u8);
+void trmR_draw_line_high(u16, u16, u16, u16, u8, u8, u8, u8, u8, u8);
 void trmR_draw_line(u16, u16, u16, u16, u8, u8, u8, u8, u8, u8);
 
 void trmR_start_bg_color(u8 r, u8 g, u8 b) {
