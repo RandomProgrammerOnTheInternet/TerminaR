9acbfda8f3b342c408079b9b810967b7cabe2e83
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2024-07-14 17:55:12 -0400

    Update demo.c

--- demo/demo.c
+++ demo/demo.c
@@ -11,6 +11,8 @@ int main() {
 	trmR_stpx(0, 50, 255, 0, 255);
 	// draw a line from (0, 0) to (100, 100) with a gradient from black to red
 	trmR_draw_line(0, 0, 100, 100, 0, 0, 0, 255, 0, 0);
+	// show the screen
+	trmR_show_scr();
 	// trmR_end isnt called because we want the image to stay
 
 	return 0;
