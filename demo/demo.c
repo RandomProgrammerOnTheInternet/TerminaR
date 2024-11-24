77c9fd996dcdeb39f084928aa378572f7f2da02b
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2024-11-24 11:11:39 -0500

    Update demo.c

--- demo/demo.c
+++ demo/demo.c
@@ -7,8 +7,8 @@
 int main() {
 	// initialize screen with RGB color white
 	trmR_init_scr(255, 255, 255);
-	// set a pixel at (50, 0) with RGB color purple, note how it is ordered (y, x)
-	trmR_stpx(0, 50, 255, 0, 255);
+	// set a pixel at (50, 0) with RGB color purple
+	trmR_stpx(50, 0, 255, 0, 255);
 	// draw a line from (0, 0) to (100, 100) with a gradient from black to red
 	trmR_draw_line(0, 0, 100, 100, 0, 0, 0, 255, 0, 0);
 	// show the screen
